edadb41228305f3f01c789cdfc71916f6e2c8ad1
KayleeChan <kayleec55@nycstudents.net>
2025-09-26 12:34:19 -0400

    initialized methods

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+public class AppointmentBook
+{
+    private boolean[][] schedule;
 
-public class AppointmentBook {
+    public AppointmentBook(boolean[][] schedule)
+    {
+        this.schedule = schedule;
+    }
+
+    private boolean isMinuteFree(int period, int minute)
+    {
+        return schedule[period - 1][minute];
+    }
+
+    public int findFreeBlock(int period, int duration)
+    {
+        return 0;
+    }
+
+    public boolean makeAppointment(int startperiod, int endPeriod, int duation)
+    {
+        return false;
+    }
 }
